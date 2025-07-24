d3f2731d874ca3e74ee8ff940ee0ca592f9863c5
James kamz <Jacquesamouzou9@gmail.com>
2025-07-24 23:57:35 +0000

    da2

---
+++ day_2/variables.py
@@ -0,0 +1,57 @@
+#Jour2: 30 Days of Python Programming
+
+# niveau 1
+#defining variables
+first_name="James"
+last_name="Kamz"
+full_name="James Kamz"
+country="Togo"
+city="Lome"
+age=10
+is_married=False
+is_true=True
+is_light_on=False
+
+# multiple variable declaration
+x, y, z = 5, 10, 15
+
+# niveau 2
+# Vérification du type de données de toutes les variables
+print("Type de prénom:", type(prenom))
+print("Type de nom de famille:", type(nom_de_famille))
+print("Type de nom complet:", type(nom_complet))
+print("Type de pays:", type(pays))
+print("Type de ville:", type(ville))
+print("Type d'âge:", type(age))
+print("Type de est_marie:", type(est_marie))
+print("Type de is_true:", type(is_true))
+print("Type de is_light_on:", type(is_light_on))
+
+# Longueur du prénom
+print("Longueur du prénom:", len(prenom))
+
+# Comparaison de la longueur du prénom et du nom de famille
+print("Prénom plus long que nom de famille ?", len(prenom) > len(nom_de_famille))
+
+# Déclaration des variables numériques
+num_one = 5
+num_two = 4
+
+# Opérations mathématiques
+somme = num_one + num_two
+difference = num_one - num_two
+produit = num_one * num_two
+division = num_one / num_two
+reste = num_one % num_two
+division_entiere = num_one // num_two
+
+# Affichage des résultats
+print("Somme:", somme)
+print("Différence:", difference)
+print("Produit:", produit)
+print("Division:", division)
+print("Reste (modulo):", reste)
+print("Division entière (floor division):", division_entiere)
+
+# Niveau 3
+d
