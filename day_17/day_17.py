a0d5e5a0739cc05feaa5354457b1cefcd2283411
James kamz <Jacquesamouzou9@gmail.com>
2025-08-10 07:52:45 +0000

    update

--- day_17/day_17.py
+++ day_17/day_17.py
@@ -1,1 +1,6 @@
 # Day 17 of 30 days with python
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+*nordic_countries, es, ru =names
+print(f'The first five countries are: {nordic_countries} es is {es} and ru is {ru}.')
