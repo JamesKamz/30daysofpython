d8e13acbb3d8491cb298380aa34a3423434a9444
James kamz <Jacquesamouzou9@gmail.com>
2025-08-14 14:02:38 +0000

    update day 23

---
+++ day_23/day_23.py
@@ -0,0 +1,18 @@
+# Day 23 of 30 days with python
+
+# Pip install virtualenv
+
+#creating a virtual environment
+# virtualenv venv
+
+# activating the virtual environment
+# source venv/bin/activate
+
+# installing flask
+# pip install flask
+
+#collecting dependencies
+# pip freeze > requirements.txt
+
+#deactivating the virtual environment
+# deactivate
